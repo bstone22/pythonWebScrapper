dc769c3db326f29e96a200b07cfeec1144b9d561
Brent Stone <bstone22@uw.edu>
2018-09-04 22:30:57 -0700

    updated user input

--- ffball/website/urls.py
+++ ffball/website/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from website import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('', views.response, name='respnse')
 ]
